2c68e79d1d995c35a6dc460addc28fae51609d8f
Alexandr Arefev <alexarefev@ya.ru>
2021-10-05 21:27:16 +0300

    API fix

--- test/test_one.sh
+++ test/test_one.sh
@@ -2,27 +2,29 @@
 
 psql -d pgmgmt -U postgres -c "TRUNCATE databases RESTART IDENTITY;"
 
-HS=${2:-pg4}
+HS=${2:-pg2}
 TP=${1:-pg}
 RESP=./response
 
+rm -Rf $RESP
 
 echo "TP=${TP}; HS=${HS}"
 
 i=1
-while [ $i -le 10 ]
+while [ $i -le 4 ]
 do
 	DB_NAME='db'$i
 	USER_NAME='user_'$i
 	let i++
 #	echo "DB=${DB_NAME}; USER=${USER_NAME}"
-	curl -XPOST -H "Authorization: Basic bGFiYXo6cnR5ZGZnNDU2" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"host\":\"$HS\", \"name\":\"$DB_NAME\", \"user\":\"$USER_NAME\"}" http://localhost:8080/api/v1/$TP >> $RESP
+	curl -XPOST -s -H "Authorization: Basic bGFiYXo6cnR5ZGZnNDU2" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"host\":\"$HS\", \"name\":\"$DB_NAME\", \"user\":\"$USER_NAME\"}" http://localhost:8080/api/v1/$TP >> $RESP
+	echo -n -e "\n" >> $RESP
 done
 
 read
 
 let i=1
-while [ $i -le 5 ]
+while [ $i -le 2 ]
 do
 	DB_NAME='db'$i
 	PASS=$(grep $DB_NAME $RESP | sed 's/.\+"password":\ "\(.\+\)"}/\1/')
@@ -32,7 +34,7 @@ do
 done
 
 let i=6
-while [ $i -le 10 ]
+while [ $i -le 4 ]
 do
 	DB_NAME='db'$i
 	PASS=$(grep $DB_NAME $RESP | sed 's/.\+"password":\ "\(.\+\)"}/\1/')
@@ -40,7 +42,3 @@ do
 #	echo "DB=${DB_NAME}"
 	curl -XDELETE -H "Authorization: Basic bGFiYXo6cnR5ZGZnNDU2" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"name\":\"$DB_NAME\", \"backup\":\"false\", \"pass\":\"$PASS\"}" http://localhost:8080/api/v1/$TP
 done
-
-
-curl -v -XPUT -H "Authorization: Basic bGFiYXo6cnR5ZGZnNDU2" -d @test_dump_pg http://localhost:8080/api/v1/backup/pg/pg1
-
